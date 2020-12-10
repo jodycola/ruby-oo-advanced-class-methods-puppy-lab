b342023182710350522793299351e0e6a02682b8
Kody Samaroo <github email address>
2020-12-10 15:07:09 -0500

    commit tree 6aced24b27f031c927e6b3ea8bb13989c551199d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def Dog.print_all
+        @@all.each { |x| puts x.name }
+    end
+
+    def save
+        @@all << self
+    end
+end
+
+# binding.pry
